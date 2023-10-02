40b89528ab7baca5d7428add0f3cfcbb2bc42c2f
Vincent Thai <vincentthai@live.ca>
2023-10-01 23:00:03 -0400

    Got a "$hello" function to work

--- main.py
+++ main.py
@@ -2,18 +2,22 @@ import discord
 from discord.ext import commands
 
 # Initializing the command prefix of the bot to be /
-client = commands.Bot(command_prefix="/")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = commands.Bot (command_prefix = '$', intents=intents)
 
 @client.event
 async def on_ready():
     print("Im ready!!")
     print("----------------")
 
-@client.command
-async def test(ctx):
+@client.command()
+async def hello(ctx):
     await ctx.send("This is working")
 
-client.run()
+client.run("MTE1ODIyMzIyMTQwMTg1NDAyMg.GsrmSZ.Ta2AjyKpmoxPJGLV9FNt5BKZxeALVZ30PJyg8o")
 
 
 
