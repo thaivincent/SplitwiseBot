f912e01cc8828aa94f5a960fae01d3ef21664e27
Vincent Thai <vincentthai@live.ca>
2023-10-08 18:33:07 -0400

    Adding function to list all members of the server

--- main.py
+++ main.py
@@ -9,6 +9,16 @@ intents.message_content = True
 
 client = commands.Bot (command_prefix = '$', intents=intents)
 
+role_exclude =  discord.utils.get()
+
+# Initialize the list of members in the group
+group_list=[]
+members = ctx.guild.members
+
+for i in members:
+    group_list.append(i.name)
+    print("added:" + i.name)
+
 @client.event
 async def on_ready():
     print("Im ready!!")
@@ -17,5 +27,10 @@ async def on_ready():
 @client.command()
 async def hello(ctx):
     await ctx.send("This is working")
+
+@client.command()
+async def list_members(ctx):
+    await ctx.send("List of members:")
+    await ctx.send(",".join(group_list))
 
 client.run(config.token)
