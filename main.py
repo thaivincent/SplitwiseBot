478af51945c1f6706abbf0dd6bbe1a56352bdbeb
Vincent Thai <vincentthai@live.ca>
2023-10-08 21:30:07 -0400

    Fixed so only members with the "Splitwise" tab are included

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ intents = discord.Intents.all()
 # Initializing the command prefix of the bot to be $
 client = commands.Bot (command_prefix = '$', intents=intents)
 
-# Initializing a an group of excluded members
-excluded_list=[]
-
 @client.event
 async def on_ready():
+    global server_guild
+    global member_list
+
     # where server_guild is a class(Guild) and represents the server.
     server_guild = client.get_guild(config.guild_id)
+
     # This is a list of "Member" objects
-    global member_list 
     member_list = server_guild.members
 
     print("Im ready!!")
@@ -29,12 +29,12 @@ async def hello(ctx):
 
 @client.command()
 async def list_members(ctx):
+    exclusive_id = discord.utils.get(server_guild.roles, name="Splitwise")
     # Adding all of the names of server members into a list
     name_list = []
-    for i in member_list  :
-        name_list.append(i.name)
+    for member in member_list:
+        if exclusive_id in member.roles:
+            name_list.append(member.name)
     await ctx.send(str(name_list)[1:-1])
         
-    
-
 client.run(config.token)
