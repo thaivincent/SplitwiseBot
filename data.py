41869feb075b30490db55da9cf4e65d8e9b849cc
Vincent Thai <vincentthai@live.ca>
2023-10-08 22:16:37 -0400

    Added a json to keep track of running balances

---
+++ data.py
@@ -0,0 +1,67 @@
+import json
+
+balance = {
+    "Vincent": {
+        "Bal1": 0,
+        "Bal2": 0,
+        "Bal3": 0,
+        "Bal4": 0,
+        "Bal5": 0,
+        "Bal6": 0,
+    },
+    "Leo": {
+        "Bal1": 0,
+        "Bal2": 0,
+        "Bal3": 0,
+        "Bal4": 0,
+        "Bal5": 0,
+        "Bal6": 0,
+    },
+    "Jeffery": {
+        "Bal1": 0,
+        "Bal2": 0,
+        "Bal3": 0,
+        "Bal4": 0,
+        "Bal5": 0,
+        "Bal6": 0,
+    },
+    "Saarukan": {
+        "Bal1": 0,
+        "Bal2": 0,
+        "Bal3": 0,
+        "Bal4": 0,
+        "Bal5": 0,
+        "Bal6": 0,
+    },
+    "Zion": {
+        "Bal1": 0,
+        "Bal2": 0,
+        "Bal3": 0,
+        "Bal4": 0,
+        "Bal5": 0,
+        "Bal6": 0,
+    },
+    "Yousif": {
+        "Bal1": 0,
+        "Bal2": 0,
+        "Bal3": 0,
+        "Bal4": 0,
+        "Bal5": 0,
+        "Bal6": 0,
+    },
+    "Ali": {
+        "Bal1": 0,
+        "Bal2": 0,
+        "Bal3": 0,
+        "Bal4": 0,
+        "Bal5": 0,
+        "Bal6": 0,
+    },
+}
+
+with open('balances.json', 'w') as json_file:
+    json.dump(balance, json_file, indent=4)
+
+# Step 3: Load the JSON file back into a variable
+with open('balances.json', 'r') as json_file:
+    loaded_balance = json.load(json_file)
--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 import discord
 from discord.ext import commands
 import config
-
+import json
 
 # Setting permissions of the bot
 intents = discord.Intents.all()
 
 # Initializing the command prefix of the bot to be $
 client = commands.Bot (command_prefix = '$', intents=intents)
+
+# This class represents the running balance of everybody in the server.
+# the ith bal coresponds to the ith person in the memberlist, excluding yourself.
+# This is custom tailored to my server of 7 people. (yeah this is trash)
+
+class Balance:
+    def __init__(self,bal1,bal2,bal3,bal4,bal5,bal6):
+        self.bal1 = bal1
+        self.bal2 = bal2
+        self.bal3 = bal3
+        self.bal4 = bal4
+        self.bal5 = bal5
+        self.bal6 = bal6
+    
+    # A function which returns the total balance you owe to everyone.
+    def totbal(self):
+        return self.bal1 + self.bal2 + self.bal3 + self.bal4 + self.bal5 + self.bal6
+    
+
+
 
 @client.event
 async def on_ready():
@@ -36,5 +56,8 @@ async def list_members(ctx):
         if exclusive_id in member.roles:
             name_list.append(member.name)
     await ctx.send(str(name_list)[1:-1])
+    print(name_list)
+
+
         
 client.run(config.token)
